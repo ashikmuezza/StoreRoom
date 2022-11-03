74d5d0aff03151d5eb88f6259952f6854216cc7b
AshikMuezza <ashik@bitscrunch.com>
2022-11-03 16:48:10 +0530

    Upload changes

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,38 +1,37 @@
-import React from 'react'
-import { TextareaAutosize } from '@mui/material';
-import { makeStyles } from '@material-ui/styles';
+import React from "react";
+import { TextareaAutosize } from "@mui/material";
+import { makeStyles } from "@material-ui/styles";
 
-import './Input.css'
+import "./Input.css";
 
 const handleSubmit = async (e) => {
   e.preventDefault();
-
 };
 
 const useStyles = makeStyles({
-    field: {
-      backgroundColor: "#dae7f5",
-      fontFamily: "Manrope",
-      color:"black",
-    }});
+  field: {
+    backgroundColor: "#042c54",
+    fontFamily: "Manrope",
+    color: "#fa816b",
+  },
+});
 
-
-    const Input = () => {
-    const classes = useStyles()
+const Input = () => {
+  const classes = useStyles();
   return (
-        <div className='input'>
-             <TextareaAutosize className={classes.field}
-                aria-label="minimum height"
-                minRows={10}
-                placeholder="Give your input"
-            />
+    <div className="input">
+      <TextareaAutosize
+        className={classes.field}
+        aria-label="minimum height"
+        minRows={10}
+        placeholder="Give your input"
+      />
 
-          <div className="input-button">
-              <button onClick={handleSubmit}>Create paste</button>
-          </div>
+      <div className="input-button">
+        <button onClick={handleSubmit}>Create paste</button>
+      </div>
+    </div>
+  );
+};
 
-        </div>
-  )
-}
-
-export default Input
+export default Input;
