478695df0c3c1566b430169e85b9ece2dd4880b3
AshikMuezza <ashik@bitscrunch.com>
2022-11-18 04:52:31 +0530

    listen added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import React from 'react'
+import React from "react";
 
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import {Feature,Navbar,Brand,Menu1,RainbowKit, Carousel} from './components'
-import {Header,Footer} from './containers'
-import './App.css'
-import {Home, Ipfs} from './pages';
-
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import {
+  Feature,
+  Navbar,
+  Brand,
+  Menu1,
+  RainbowKit,
+  Carousel,
+} from "./components";
+import { Header, Footer } from "./containers";
+import "./App.css";
+import { Home, Ipfs } from "./pages";
 
 const App = () => {
   return (
     <BrowserRouter>
-    <div className="App">
-          <Routes>
-          <Route path="/" element={<Home/>} default />
-          <Route path="/chains/:title"  element={<Ipfs/>} /> 
-      </Routes>
-    </div>
+      <div className="App">
+        <Routes>
+          <Route path="/" element={<Home />} default />
+          <Route path="/chains/:title" element={<Ipfs />} />
+        </Routes>
+      </div>
     </BrowserRouter>
-  )
-}
+  );
+};
+
+App.listen(8080, () => {
+  console.log("listening on port 8080");
+});
 
 export default App;
